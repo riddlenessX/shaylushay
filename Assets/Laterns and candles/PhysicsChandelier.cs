b1cb44dced049476e4152c988eeaa6501992a07c
nukkumaanmenoaika <misha_bro@list.ru>
2023-10-16 20:54:10 +0900

    very scary

---
+++ Assets/Laterns and candles/PhysicsChandelier.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsChandelier : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody PhysicsPrefab;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        PhysicsPrefab.isKinematic = false;
+    }
+}
